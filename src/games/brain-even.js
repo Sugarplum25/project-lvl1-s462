0ea4909169628f6e2ffeab680eb2263ddb8f1e8f
Maria <Sugarplamka@gmai.com>
2019-03-20 19:39:45 +0300

    fix one linter mistake, change export

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,16 +1,16 @@
 import readlineSync from 'readline-sync';
 
-const generateNumber = () => Math.floor(Math.random() * 1000);
-const isEven = number => number % 2 === 0;
-const gameRound = 2;
+export const generateNumber = () => Math.floor(Math.random() * 1000);
+export const isEven = number => number % 2 === 0;
 
-
-const evenGame = () => {
+export const evenGame = () => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name?');
   console.log(`Hello, ${userName}!`);
   console.log('Answer "yes" if number even otherwise answer "no"');
+  const gameRound = 2;
   const letsPlayEven = (gameRound) => {
+
     const gameQuestion = generateNumber();
 
     const rightAnswear = isEven(gameQuestion) ? 'yes' : 'no';
@@ -20,16 +20,14 @@ const evenGame = () => {
     const userAnswear = readlineSync.question('Your answer: ');
 
     if (gameRound === 0) {
-      console.log(`Congratulations, ${userName}!`);
-      return;
+      return console.log(`Congratulations, ${userName}!`);
     }
     console.log(`Correct answer was ${rightAnswear}`);
     if (userAnswear === rightAnswear) {
       console.log('Correct!');
       return letsPlayEven(gameRound - 1);
     }
-    console.log(`${userAnswear} is wrong answer ;(. Correct answer was ${rightAnswear}. Let's try again, ${userName}`);
+    return console.log(`${userAnswear} is wrong answer ;(. Correct answer was ${rightAnswear}. Let's try again, ${userName}`);
   };
   letsPlayEven(gameRound);
 };
-export default evenGame;
