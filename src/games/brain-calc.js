db1c2973cdc966647b05afb45ef62fa3262ffcbc
Maria <Sugarplamka@gmai.com>
2019-03-21 22:06:16 +0300

    working version

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -1,57 +1,52 @@
-// Помоги, пожалуйста, разобраться с кодом. 
-// Не получается на последующей итерации вернуть новое выражение, все время возвращается одно и то же.
-// Я совсем запуталась....
-
-
 import readlineSync from 'readline-sync';
 
-const numberGenerator = () => Math.floor(Math.random() * 10);
-const numberOne = numberGenerator();
-const numberTwo = numberGenerator();
+import { generateNumber } from '../support';
+
 const listOfOperators = ['+', '-', '*'];
 const findRandomOperator = Math.floor(Math.random() * listOfOperators.length);
-const randomOperator = listOfOperators[findRandomOperator];
-const generateQuestion = () => (`${numberOne} ${randomOperator} ${numberTwo}`);
-
-const pickExpression = (char) => {
-  let resultOfExpression;
-  switch (char) {
-    case '+':
-      resultOfExpression = numberOne + numberTwo;
-      break;
-    case '-':
-      resultOfExpression = numberOne - numberTwo;
-      break;
-    case '*':
-      resultOfExpression = numberOne * numberTwo;
-      break;
-    default:
-      break;
-  }
-  return resultOfExpression;
-};
 
 const calcGame = () => {
   const gameRound = 3;
   console.log('Welcome to the Brain Games!');
+  console.log();
   const userName = readlineSync.question('May I have your name?');
   console.log(`Hello, ${userName}!`);
+  console.log();
   console.log('What is the result of the expression?');
+  console.log();
 
   const iterRound = (round) => {
-    console.log(`Question: ${generateQuestion()}`);
-
-    const trueAnswear = String(pickExpression(randomOperator));
-
-    console.log(typeof trueAnswear);
-
     if (round === 0) {
       return console.log(`Congratulations, ${userName}!`);
     }
+    const numberOne = generateNumber();
+    const numberTwo = generateNumber();
+    const randomOperator = listOfOperators[findRandomOperator];
+    const generateQuestion = () => (`${numberOne} ${randomOperator} ${numberTwo}`);
+    const pickExpression = (char) => {
+      let resultOfExpression;
+      switch (char) {
+        case '+':
+          resultOfExpression = numberOne + numberTwo;
+          break;
+        case '-':
+          resultOfExpression = numberOne - numberTwo;
+          break;
+        case '*':
+          resultOfExpression = numberOne * numberTwo;
+          break;
+        default:
+          break;
+      }
+      return resultOfExpression;
+    };
+
+    const gameQuestion = generateQuestion();
+    console.log(`Question: ${gameQuestion}`);
+
+    const trueAnswear = String(pickExpression(randomOperator));
 
     const userResponce = (readlineSync.question('Your answer: '));
-
-    console.log(typeof userResponce);
 
     console.log(`Correct answer was ${trueAnswear}`);
 
@@ -59,7 +54,8 @@ const calcGame = () => {
       console.log('Correct!');
       return iterRound(round - 1);
     }
-    return console.log(`${userResponce} is wrong answer ;(. Correct answer was ${trueAnswear}. Let's try again, ${userName}`);
+    console.log(`${userResponce} is wrong answer ;(. Correct answer was ${trueAnswear}.`);
+    console.log(`Let's try again, ${userName}`);
   };
 
   return iterRound(gameRound);
