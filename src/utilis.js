6845f54d569ecb36dbbf376bc78d8d879e15c602
Maria <Sugarplamka@gmai.com>
2019-03-22 16:56:33 +0300

    commit

--- src/utilis.js
+++ src/utilis.js
@@ -1,4 +1,4 @@
 
-export const generateNumber = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+const generateNumber = (min, max) => (Math.floor(Math.random() * (max + 1 - min)) + min);
 
-export const isEven = number => number % 2 === 0;
+export default generateNumber;
