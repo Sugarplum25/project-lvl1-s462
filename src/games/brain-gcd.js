e7e6de4a9e266d8c7fd8207d3462ec3492558197
Maria <Sugarplamka@gmai.com>
2019-03-23 13:04:01 +0300

    fix names

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -1,24 +1,22 @@
-import gamePlay from '..';
+import playGame from '..';
 
 import generateNumber from '../utilis';
 
-const gcd = (num1, num2) => {
+const getGcd = (num1, num2) => {
   if (!num2) {
     return num1;
   }
-  return gcd(num2, num1 % num2);
+  return getGcd(num2, num1 % num2);
 };
+
 const gameName = 'Find the greatest common divisor of given numbers';
 
-const gameQuestion = () => {
+const getQuestion = () => {
   const numberOne = generateNumber(1, 100);
   const numberTwo = generateNumber(1, 100);
-  const askQuestion = `${numberOne} and ${numberTwo}`;
-  const rightAnswear = String(gcd(numberOne, numberTwo));
-  return {
-    askQuestion,
-    rightAnswear,
-  };
+  const question = `${numberOne} and ${numberTwo}`;
+  const rightAnswear = String(getGcd(numberOne, numberTwo));
+  return { question, rightAnswear };
 };
 
-export default () => gamePlay(gameQuestion, gameName);
+export default () => playGame(getQuestion, gameName);
