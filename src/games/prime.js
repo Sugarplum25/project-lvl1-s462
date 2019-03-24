6d28e7d6f51a88cfc094149b3d807638c5e8d4e5
Maria <Sugarplamka@gmai.com>
2019-03-24 16:13:05 +0300

    fix function

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,12 +1,11 @@
 import playGame from '..';
-
 import generateNumber from '../utilis';
 
 const isPrime = (num) => {
   if (num < 2) {
     return false;
   }
-  for (let i = 2; i < num; i += 1) {
+  for (let i = 2; i < num / 2; i += 1) {
     if (num % i === 0) {
       return false;
     }
